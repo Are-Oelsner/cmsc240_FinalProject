1b7264ea354ec5e194a413fa72173a603fcdb162
RyanAJennings <ryanajennings11098@gmail.com>
2017-12-15 22:41:21 -0500

    Cleanup TrafficLight & Parser

--- TrafficLight.h
+++ TrafficLight.h
@@ -2,42 +2,73 @@
 //* Authors: Are Oelsner, Maddie Shea, Ryan Jennings
 //* Date: 
 
-#ifndef __traffic_h__
-#define __traffic_h__
+#ifndef __TRAFFIC_H__
+#define __TRAFFIC_H__
 
-
-class TrafficLight
-{
-public:
-    
-    //traffic color enum
-    enum Color { red, green, yellow};
-    
-    //constructor
-    TrafficLight(int green, int red, int yellow);
-    
-    void change(Color newColor);
-    
-    //getters
-    int getGreenTime();
-    int getYellowTime();
-    int getRedTime();
-    
-    //get color
-    Color getColor();
-    
-    
-    //remaining time
-    int timeRemaining(Color color, int timeElapsed);
+//******************************************************************************
+//* TODO - Class description
+class TrafficLight {
 
 protected:
-	//time
-	int greenTime; 
-	int yellowTime;
-	int redTime;
 
-	//variable to store current state
-	Color currentColor;
+	//**************************************************************************
+    //* Private Variables
+
+	Color currentColor;	//* Current color of the TrafficLight
+
+	//* Times
+	int greenTime; 	//* Time duration of a green light
+	int yellowTime;	//* Time duration of a yellow light
+	int redTime;	//* Time duration of a red light
+
+public:
+
+	//**************************************************************************
+    //* Public Variables
+    
+    //* Traffic color enum
+    enum Color { red, green, yellow };
+    
+    //**************************************************************************
+    //* Constructors & Destructors
+
+    //* Default Constructor
+    TrafficLight();
+
+    //* Constructor
+    TrafficLight(int green, int red, int yellow);
+
+    //* Destructor
+    ~TrafficLight();
+
+    //**************************************************************************
+    //* Functions
+    
+    //* Changes the current color of the TrafficLight
+    //* @param 	newColor 	Color to change the TrafficLight to
+    void change(Color newColor);
+    
+    //**************************************************************************
+    //* Get Functions
+
+    //* Returns the time duration of a green light
+    //* @return integer time duration of a green light
+    int getGreenTime();
+
+    //* Returns the time duration of a yellow light
+    //* @return integer time duration of a yellow light
+    int getYellowTime();
+
+    //* Returns the time duration of a red light
+    //* @return integer time duration of a red light
+    int getRedTime();
+    
+    //* Returns the current color of the TrafficLight
+    //* @return  Color 	current color of the TrafficLight
+    Color getColor();
+    
+    //* Remaining time duration of the current color
+    int timeRemaining(int timeElapsed);
 
 };
 
--- parser.h
+++ parser.h
@@ -10,52 +10,81 @@
 
 using namespace std;
 
-
+//******************************************************************************
+//* TODO - Class description
 class Parser {
 
 protected:
 
-	double left_prob;
-	double right_prob;
+    //**************************************************************************
+    //* Private Variables
+
+	double left_prob;      //* Probability that a Vehicle will turn left
+	double right_prob;     //* Probability that a Vehicle will turn right
     
 
-	double g_ticks;
-	double y_ticks;
-	double r_ticks;
+	double g_ticks;        //* Number of ticks for a green light
+	double y_ticks;        //* Number of ticks for a yellow light
+	double r_ticks;        //* Number of ticks for a red light
 
+	double car_prob;       //* Probability that a Vehicle will spawn as a Car
+    double SUV_prob;       //* Probability that a Vehicle will spawn as a SUV
+	double truck_prob;     //* Probability that a Vehicle will spawn as a Truck
 
-	double car_prob;
-    double SUV_prob;
-	double truck_prob;
+    int section_num;        //* Number of Sections in a Lane
+    vector<string> info1;   //* TODO
 
-    int section_num;
-    vector<string> info1;
 public:
 
+    //**************************************************************************
+    //* Functions
 
+    //* Parses information from the file given the filename
+    //* @param  string   Name of the file to parse from
 	void parseConfigFile(string filename); 
     
-    //get methods for the variables
 
+    //**************************************************************************
+    //* Get Functions
+
+    //* Returns the probability that a Vehicle will turn left
+    //* @return     double probability that a Vehicle will turn left
     double getLeft_Prob();
     
+    //* Returns the probability that a Vehicle will turn left
+    //* @return     double probability that a Vehicle will turn left
     double getRight_Prob();
     
+    //* Returns the probability that a Vehicle will remain straight
+    //* @return     double probability that a Vehicle will remain straight
     double getStraight_Prob();
     
-
+    //* Returns the number of ticks for a green light
+    //* @return     double number of ticks for a green light
     double getG_Ticks();
 
+    //* Returns the number of ticks for a yellow light
+    //* @return     double number of ticks for a yellow light
     double getY_Ticks();
 
+    //* Returns the number of ticks for a red light
+    //* @return     double number of ticks for a red light
     double getR_Ticks();
     
+    //* Returns the probability that a Vehicle will be spawned as a Car
+    //* @return     double probability that a Vehicle will be spawned as a Car
     double getCar_Prob();
 
+    //* Returns the probability that a Vehicle will be spawned as a SUV
+    //* @return     double probability that a Vehicle will be spawned as a SUV
     double getSUV_Prob();
 
+    //* Returns the probability that a Vehicle will be spawned as a Truck
+    //* @return     double probability that a Vehicle will be spawned as a Truck
     double getTruck_Prob();
 
+    //* Returns the number of Sections in a Lane
+    //* @return     integer number of Sections in a Lane
     int getSection_Num();
 
 };
