de47ae65d382396a2df0d0836aaac1fadda33fec
areo279 <are.oelsner@richmond.edu>
2017-12-16 16:26:47 -0500

    Are -updated Section Tester but having a weird error

--- SectionTester.cpp
+++ SectionTester.cpp
@@ -6,54 +6,55 @@ using namespace std;
 
 int main() {
 	// Testing Default Constructor
-	Section::Section section = new Section();
+	Section* section = new Section();
+
 	// Testing Default Traffic Flow
-	cout << "getNorth(): " << section.getNorth() << ". [false]" << endl;
-	cout << "getEast(): " << section.getEast() << ". [false]" << endl;
-	cout << "getSouth(): " << section.getSouth() << ". [false]" << endl;
-	cout << "getWest(): " << section.getWest() << ". [false]" << endl;
+	cout << "getNorth(): " << section->getNorth() << ". [false]" << endl;
+	cout << "getEast(): " << section->getEast() << ". [false]" << endl;
+	cout << "getSouth(): " << section->getSouth() << ". [false]" << endl;
+	cout << "getWest(): " << section->getWest() << ". [false]" << endl;
 
 	// Testing Neighbors on Default //TODO this might not print null
-	cout << "getUpSection(): " << section.getUpSection() << ". [NULL]" << endl;
-	cout << "getRightSection(): " << section.getRightSection() << ". [NULL]" << endl;
-	cout << "getDownSection(): " << section.getDownSection() << ". [NULL]" << endl;
-	cout << "getLeftSection(): " << section.getLeftSection() << ". [NULL]" << endl;
+	cout << "getUpsection(): " << section->getUpSection() << ". [NULL]" << endl;
+	cout << "getRightSection(): " << section->getRightSection() << ". [NULL]" << endl;
+	cout << "getDownSection(): " << section->getDownSection() << ". [NULL]" << endl;
+	cout << "getLeftSection(): " << section->getLeftSection() << ". [NULL]" << endl;
 
 	// Testing Getters on Default
-	cout << "getOccupied(): " << section.getOccupied() << ". [false]" << endl;
-	cout << "getNearIntersection(): " << section.getNearIntersection() << ". [false]" << endl;
-	cout << "getNearEdge(): " << section.getNearEdge() << ". [false]" << endl;
+	cout << "getOccupied(): " << section->getOccupied() << ". [false]" << endl;
+	cout << "getNearIntersection(): " << section->getNearIntersection() << ". [false]" << endl;
+	cout << "getNearEdge(): " << section->getNearEdge() << ". [false]" << endl;
 
 	// Creating test neighbor sections
-	Section::Section uNeighbor = new Section();
-	Section::Section rNeighbor = new Section();
-	Section::Section dNeighbor = new Section();
-	Section::Section lNeighbor = new Section();
+	Section* uNeighbor = new Section();
+	Section* rNeighbor = new Section();
+	Section* dNeighbor = new Section();
+	Section* lNeighbor = new Section();
 
 	// Testing Setters
-	section.setNeighbor(uNeighbor, 1);
-	section.setNeighbor(rNeighbor, 2);
-	section.setNeighbor(dNeighbor, 3);
-	section.setNeighbor(lNeighbor, 4);
+	section->setNeighbor(uNeighbor, 1);
+	section->setNeighbor(rNeighbor, 2);
+	section->setNeighbor(dNeighbor, 3);
+	section->setNeighbor(lNeighbor, 4);
 
-	section.setOccupied(true);
-	section.setNearEdge(true);
-	section.setNearIntersection(true);
+	section->setOccupied(true);
+	section->setNearEdge(true);
+	section->setNearIntersection(true);
 	
 	// Testing Traffic Flow
-	cout << "getNorth(): " << section.getNorth() << ". [true]" << endl;
-	cout << "getEast(): " << section.getEast() << ". [true]" << endl;
-	cout << "getSouth(): " << section.getSouth() << ". [true]" << endl;
-	cout << "getWest(): " << section.getWest() << ". [true]" << endl;
+	cout << "getNorth(): " << section->getNorth() << ". [true]" << endl;
+	cout << "getEast(): " << section->getEast() << ". [true]" << endl;
+	cout << "getSouth(): " << section->getSouth() << ". [true]" << endl;
+	cout << "getWest(): " << section->getWest() << ". [true]" << endl;
 
 	// Testing Neighbors //TODO might need to write print method 
-	cout << "getUpSection(): " << section.getUpSection() << ". [something]" << endl;
-	cout << "getRightSection(): " << section.getRightSection() << ". [something]" << endl;
-	cout << "getDownSection(): " << section.getDownSection() << ". [something]" << endl;
-	cout << "getLeftSection(): " << section.getLeftSection() << ". [something]" << endl;
+	cout << "getUpSection(): " << section->getUpSection() << ". [something]" << endl;
+	cout << "getRightSection(): " << section->getRightSection() << ". [something]" << endl;
+	cout << "getDownSection(): " << section->getDownSection() << ". [something]" << endl;
+	cout << "getLeftSection(): " << section->getLeftSection() << ". [something]" << endl;
 
 	// Testing Getters 
-	cout << "getOccupied(): " << section.getOccupied() << ". [true]" << endl;
-	cout << "getNearIntersection(): " << section.getNearIntersection() << ". [true]" << endl;
-	cout << "getNearEdge(): " << section.getNearEdge() << ". [true]" << endl;
+	cout << "getOccupied(): " << section->getOccupied() << ". [true]" << endl;
+	cout << "getNearIntersection(): " << section->getNearIntersection() << ". [true]" << endl;
+	cout << "getNearEdge(): " << section->getNearEdge() << ". [true]" << endl;
 }
