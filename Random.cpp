564c41014dd4cec9630fa062a76387cbc5cb9576
mshea14 <maddies14@outlook.com>
2017-12-17 18:37:26 -0500

    Random Double Added

--- Random.cpp
+++ Random.cpp
@@ -4,8 +4,19 @@
 
 #include "Random.h"
 
-	static int Random::randint(int a, int b) {
+	int Random::randint(int a, int b) {
 	  double u = Random::randdouble(Random::rng);
 	  int mappedValue = a + (int)((b - a + 1)*u);
 	  return mappedValue;
+	}
+
+	double Random::randDouble(double min, double max)
+	{
+    	double before = rand() % (int)max + (int)min;
+   		double after = (double)rand() / RAND_MAX;
+    	double result = before + after;
+    	if (result < min || result > max) {
+        	result = randDouble(min, max);
+    	}
+    	return result;
 	}
