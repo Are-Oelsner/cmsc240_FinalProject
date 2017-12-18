4e934e83102d3e4da8ca538e36ec4be45d029dc0
RyanAJennings <ryanajennings11098@gmail.com>
2017-12-17 19:22:48 -0500

    Cleanup Random

--- Random.cpp
+++ Random.cpp
@@ -3,21 +3,3 @@
 //* Date:
 
 #include "Random.h"
-/*
-	int Random::randint(int a, int b) {
-	  double u = Random::randdouble(Random::rng);
-	  int mappedValue = a + (int)((b - a + 1)*u);
-	  return mappedValue;
-	}
-
-	double Random::randDouble(double min, double max)
-	{
-    	double before = rand() % (int)max + (int)min;
-   		double after = (double)rand() / RAND_MAX;
-    	double result = before + after;
-    	if (result < min || result > max) {
-        	result = randDouble(min, max);
-    	}
-    	return result;
-	}
-*/
