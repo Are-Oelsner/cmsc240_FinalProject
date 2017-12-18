70889faa0788e762e370ecdd1165466b4f79623f
mshea14 <maddies14@outlook.com>
2017-12-18 11:26:16 -0500

    Traffic Light Update

--- Intersection.cpp
+++ Intersection.cpp
@@ -100,33 +100,34 @@ getLane(int direction) {
 
 void Intersection::update(bool debug) {
 
-  counter++;
+  counter0++;
+  counter1++;
   // Handle light timing here? Update trafficlights?
   TrafficLight nTraffic = north.getTrafficLight();
   TrafficLight eTraffic = east.getTrafficLight();
   TrafficLight sTraffic = south.getTrafficLight();
   TrafficLight wTraffic = west.getTrafficLight();
 
-  double n = nTraffic.timeRemaining(counter);
-  double e = eTraffic.timeRemaining(counter);
-  double s = sTraffic.timeRemaining(counter);
-  double w = wTraffic.timeRemaining(counter);
+  double n = nTraffic.timeRemaining(counter1);
+  double e = eTraffic.timeRemaining(counter0);
+  double s = sTraffic.timeRemaining(counter1);
+  double w = wTraffic.timeRemaining(counter0);
 
   if(n==0) {
     nTraffic.change(); 
-    counter=0;
+    counter1=0;
   }
   if(e==0) {
     eTraffic.change();
-    counter = 0;
+    counter0 = 0;
   }
   if(s==0) {
     sTraffic.change();
-    counter = 0;
+    counter1 = 0;
   }
   if(w==0) {
     wTraffic.change();
-    counter=0;
+    counter0=0;
   }
 
   for(int i = 0; i < vehicles.size(); i++) {
